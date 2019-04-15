afeaa7c39b054db739f0b9548507ca043bfe54f7
bassasaurus <clearance.clarence@gmail.com>
2019-04-15 17:24:48 -0500

    memcache settings

--- logbook/settings.py
+++ logbook/settings.py
@@ -224,6 +224,49 @@ REST_FRAMEWORK = {
 #     }
 # }
 
+servers = os.environ['MEMCACHIER_SERVERS']
+username = os.environ['MEMCACHIER_USERNAME']
+password = os.environ['MEMCACHIER_PASSWORD']
+
+CACHES = {
+    'default': {
+        # Use Django's native pylibmc backend (since Django 1.11, use
+        # django-pylibmc for older versions)
+        'BACKEND': 'django.core.cache.backends.memcached.PyLibMCCache',
+        # TIMEOUT is not the connection timeout! It's the default expiration
+        # timeout that should be applied to keys! Setting it to `None`
+        # disables expiration.
+        'TIMEOUT': None,
+        'LOCATION': servers,
+        'OPTIONS': {
+            # Use binary memcache protocol (needed for authentication)
+            'binary': True,
+            'username': username,
+            'password': password,
+            'behaviors': {
+                # Enable faster IO
+                'no_block': True,
+                'tcp_nodelay': True,
+                # Keep connection alive
+                'tcp_keepalive': True,
+                # Timeout settings
+                'connect_timeout': 2000, # ms
+                'send_timeout': 750 * 1000, # us
+                'receive_timeout': 750 * 1000, # us
+                # Timeout for set/get requests (sadly timeouts don't mark a
+                # server as failed, so failover only works when the connection
+                # is refused)
+                '_poll_timeout': 2000, # ms
+                # Better failover
+                'ketama': True,
+                'remove_failed': 1,
+                'retry_timeout': 2,
+                'dead_timeout': 30,
+            }
+        }
+    }
+}
+
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
