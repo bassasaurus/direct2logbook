9fc7301519f223b788bb1a88057d325106db84e0
bassasaurus <clearance.clarence@gmail.com>
2019-04-15 16:27:42 -0500

    set django-heroku-memcache

--- logbook/settings.py
+++ logbook/settings.py
@@ -16,6 +16,10 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+from memcacheify import memcacheify
+
+CACHES = memcacheify()
+MEMCACHEIFY_USE_LOCAL=True
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
