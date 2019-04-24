1b27bb7200c6093dd20950ad42071d72f03a5905
bassasaurus <clearance.clarence@gmail.com>
2019-04-24 09:09:45 -0700

    commented out urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,5 @@ from accounts import views
 urlpatterns = [
     url(r'^accounts/signup/$', views.signup, name='signup'),
     # url(r'^profile/$', ProfileView.as_view(), name='profile'),
-    url(r'^profile/update/(?P<pk>\d+)/$', ProfileUpdateView.as_view(), name='profile_update'),
+    # url(r'^profile/update/(?P<pk>\d+)/$', ProfileUpdateView.as_view(), name='profile_update'),
 ]
