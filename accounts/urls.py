161a8e86b78c23da9bb7cfe7e8a6ed4f99b8c2f1
bassasaurus <clearance.clarence@gmail.com>
2019-04-24 09:25:57 -0700

    changed to accounts urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,6 @@ from accounts.views import signup, ProfileView, ProfileUpdateView
 
 urlpatterns = [
     url(r'^accounts/signup/$', signup, name='signup'),
-    url(r'^profile/$', ProfileView.as_view(), name='profile'),
-    url(r'^profile/update/(?P<pk>\d+)/$', ProfileUpdateView.as_view(), name='profile_update'),
+    url(r'^accounts/profile/$', ProfileView.as_view(), name='profile'),
+    url(r'^accounts/profile/update/(?P<pk>\d+)/$', ProfileUpdateView.as_view(), name='profile_update'),
 ]
