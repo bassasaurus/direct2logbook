be4911082cb502bf82da361b07ffe049e58c2a1d
bassasaurus <clearance.clarence@gmail.com>
2019-05-29 12:20:55 -0500

    deleted static folder

--- logbook/settings/base.py
+++ logbook/settings/base.py
@@ -176,16 +176,11 @@ STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, '/media/')
 MEDIA_URL = '/media/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
