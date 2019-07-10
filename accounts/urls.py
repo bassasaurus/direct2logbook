8298499d161bfee6c7aa3cec61accacbe9932735
Blake Powell <clearance.clarence@gmail.com>
2019-07-09 19:16:19 -0500

    all_auth set up

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from accounts.views import ProfileView, ProfileUpdateView
+from accounts.views import signup, ProfileView, ProfileUpdateView
 
 urlpatterns = [
+    url(r'^accounts/signup/$', signup, name='signup'),
     url(r'^accounts/profile/$', ProfileView.as_view(), name='profile'),
     url(r'^accounts/profile/update/(?P<pk>\d+)/$', ProfileUpdateView.as_view(), name='profile_update'),
 ]
