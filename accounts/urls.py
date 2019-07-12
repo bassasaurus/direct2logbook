16306900c0c2074f349e9000450a3d83ca3e3dc1
Blake Powell <clearance.clarence@gmail.com>
2019-07-12 14:16:39 -0500

    more views and breadcrumbs

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,27 @@
 from django.conf.urls import url
-from accounts.views import signup, ProfileView, ProfileUpdateView, EmailView, ConnectionsView
+from accounts.views import signup, ProfileView, ProfileUpdateView, EmailView, ConnectionsView, PasswordSetView, PasswordChangeView, PasswordResetView, PasswordResetDoneView, PasswordResetFromKeyView, PasswordResetFromKeyDoneView
 
 urlpatterns = [
     url(r'^accounts/signup/$', signup, name='signup'),
     url(r'^accounts/profile/$', ProfileView.as_view(), name='profile'),
-    url(r'^accounts/profile/update/(?P<pk>\d+)/$', ProfileUpdateView.as_view(), name='profile_update'),
-    #override allauth urls
+    url(r'^accounts/profile/update/(?P<pk>\d+)/$',
+        ProfileUpdateView.as_view(), name='profile_update'),
+    # override allauth urls
     url(r'^accounts/email/$', EmailView.as_view(), name='account_email'),
-    url(r'^accounts/social/connections/$', ConnectionsView.as_view(), name='socialaccount_connections'),
+    url(r'^accounts/social/connections/$',
+        ConnectionsView.as_view(), name='socialaccount_connections'),
+    url(r'^accounts/password/set/$', PasswordSetView.as_view(),
+        name='account_set_password'),
+    url(r'^accounts/password/change/$', PasswordChangeView.as_view(),
+        name='account_change_password'),
+    url(r'^accounts/password/reset/$', PasswordResetView.as_view(),
+        name='account_change_password'),
+    url(r'^accounts/password/reset/done/$',
+        PasswordResetDoneView.as_view(), name='account_password_reset_done'),
+    url(r"^accounts/password/reset/key/(?P<uidb36>[0-9A-Za-z]+)-(?P<key>.+)/$",
+        PasswordResetFromKeyView.as_view(),
+        name="account_reset_password_from_key"),
+    url(r"^password/reset/key/done/$", PasswordResetFromKeyDoneView.as_view(),
+        name="account_reset_password_from_key_done"),
 
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -13,7 +13,7 @@ from django.views.generic import TemplateView, UpdateView
 from accounts.models import Profile
 from accounts.forms import ProfileForm
 
-from allauth.account.views import EmailView
+from allauth.account.views import EmailView, PasswordSetView, PasswordChangeView, PasswordResetView, PasswordResetDoneView, PasswordResetFromKeyView, PasswordResetFromKeyDoneView
 from allauth.socialaccount.views import ConnectionsView
 
 class LoginRequiredMixin(LoginRequiredMixin):
@@ -81,6 +81,7 @@ class EmailView(EmailView):
         context = super(EmailView, self).get_context_data(**kwargs)
 
         context['title'] = "D-> | Update Email"
+        context['home_link'] = reverse('home')
         context['parent_name'] = 'Profile'
         context['parent_link'] = reverse('profile')
         context['page_title'] = 'Update Email'
@@ -92,7 +93,80 @@ class ConnectionsView(ConnectionsView):
         context = super(ConnectionsView, self).get_context_data(**kwargs)
 
         context['title'] = "D-> | Update Social Accounts"
+        context['home_link'] = reverse('home')
         context['parent_name'] = 'Profile'
         context['parent_link'] = reverse('profile')
         context['page_title'] = 'Update Social Accounts'
+        return context
+
+class PasswordSetView(PasswordSetView):
+
+    def get_context_data(self, **kwargs):
+        context = super(PasswordSetView, self).get_context_data(**kwargs)
+
+        context['title'] = "D-> | Password Set"
+        context['home_link'] = reverse('home')
+        context['parent_name'] = 'Profile'
+        context['parent_link'] = reverse('profile')
+        context['page_title'] = 'Password Set'
+        return context
+
+class PasswordChangeView(PasswordChangeView):
+
+    def get_context_data(self, **kwargs):
+        context = super(PasswordChangeView, self).get_context_data(**kwargs)
+
+        context['title'] = "D-> | Password Change"
+        context['home_link'] = reverse('home')
+        context['parent_name'] = 'Profile'
+        context['parent_link'] = reverse('profile')
+        context['page_title'] = 'Passsword Change'
+        return context
+
+class PasswordResetView(PasswordResetView):
+
+    def get_context_data(self, **kwargs):
+        context = super(PasswordResetView, self).get_context_data(**kwargs)
+
+        context['title'] = "D-> | Password Reset"
+        context['home_link'] = reverse('home')
+        context['parent_name'] = 'Profile'
+        context['parent_link'] = reverse('profile')
+        context['page_title'] = 'Password Reset'
+        return context
+
+class PasswordResetDoneView(PasswordResetDoneView):
+
+    def get_context_data(self, **kwargs):
+        context = super(PasswordResetDoneView, self).get_context_data(**kwargs)
+
+        context['title'] = "D-> | Password Reset"
+        context['home_link'] = reverse('home')
+        context['parent_name'] = 'Profile'
+        context['parent_link'] = reverse('profile')
+        context['page_title'] = 'Password Reset'
+        return context
+
+class PasswordResetFromKeyView(PasswordResetFromKeyView):
+
+    def get_context_data(self, **kwargs):
+        context = super(PasswordResetFromKeyView, self).get_context_data(**kwargs)
+
+        context['title'] = "D-> | Change Password"
+        context['home_link'] = reverse('home')
+        context['parent_name'] = 'Profile'
+        context['parent_link'] = reverse('profile')
+        context['page_title'] = 'Change Password'
+        return context
+
+class PasswordResetFromKeyDoneView(PasswordResetFromKeyDoneView):
+
+    def get_context_data(self, **kwargs):
+        context = super(PasswordResetFromKeyView, self).get_context_data(**kwargs)
+
+        context['title'] = "D-> | Password Changed"
+        context['home_link'] = reverse('home')
+        context['parent_name'] = 'Profile'
+        context['parent_link'] = reverse('profile')
+        context['page_title'] = 'Password Changed'
         return context
