175f608affcd499cd6314f692f93927c57bba826
Blake Powell <clearance.clarence@gmail.com>
2019-07-15 09:37:42 -0500

    fixed signup template (#22)

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url
-from accounts.views import signup, ProfileView, ProfileUpdateView, EmailView, ConnectionsView, PasswordSetView, PasswordChangeView, PasswordResetView, PasswordResetDoneView, PasswordResetFromKeyView, PasswordResetFromKeyDoneView
+from accounts.views import ProfileView, ProfileUpdateView, EmailView, ConnectionsView, PasswordSetView, PasswordChangeView, PasswordResetView, PasswordResetDoneView, PasswordResetFromKeyView, PasswordResetFromKeyDoneView
 
 urlpatterns = [
-    url(r'^accounts/signup/$', signup, name='signup'),
+    # url(r'^accounts/signup/$', signup, name='signup'),
     url(r'^accounts/profile/$', ProfileView.as_view(), name='profile'),
     url(r'^accounts/profile/update/(?P<pk>\d+)/$',
         ProfileUpdateView.as_view(), name='profile_update'),
