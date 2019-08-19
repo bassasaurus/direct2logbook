0f73269bc0756f37b182f60cca07bc204e5110ac
Blake Powell <clearance.clarence@gmail.com>
2019-08-19 10:43:42 -0500

    update logo graphic

--- payments/views.py
+++ payments/views.py
@@ -4,10 +4,13 @@ from django.http import HttpResponse
 from decouple import config
 from django.views.decorators.csrf import csrf_exempt
 import stripe
+from django.views.generic import TemplateView
+from django.urls import reverse
 
-stripe.api_key = config('STRIPE_TEST_PUBLISHABLE_KEY')
 
-endpoint_secret = 'whsec_0PhWIPlbu61crYCy1xq0uG0QBalnesIO'
+stripe.api_key = config('STRIPE_TEST_SECRET_KEY')
+
+# endpoint_secret = 'whsec_0PhWIPlbu61crYCy1xq0uG0QBalnesIO'
 
 @csrf_exempt
 def stripe_webhook_view(request):
@@ -23,7 +26,7 @@ def stripe_webhook_view(request):
     return HttpResponse(status=400)
 
   # Handle the event
-  if event.type == 'checkout.session.completed':
+  if event.type == 'checkout.session.completed' or 'subscription_schedule.completed':
     payment_intent = event.data.object # contains a stripe.PaymentIntent
     # handle_payment_intent_succeeded(payment_intent)
   # elif event.type == 'payment_method.attached':
@@ -35,3 +38,21 @@ def stripe_webhook_view(request):
     return HttpResponse(status=400)
 
   return HttpResponse(status=200)
+
+def success_view(request):
+        context = {
+        'title': 'Success',
+        'home_link': reverse('home'),
+        'parent_link': reverse('profile'),
+        'parent_name': 'Profile'
+        }
+        return render(request,'payments/success.html', context)
+
+def canceled_view(request):
+        context = {
+        'title': 'Canceled',
+        'home_link': reverse('home'),
+        'parent_link': reverse('profile'),
+        'parent_name': 'Profile'
+        }
+        return render(request,'payments/canceled.html', context)
