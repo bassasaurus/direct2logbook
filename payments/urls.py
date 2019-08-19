e55c4994153843d3833b3ff5405e1174ef248a71
Blake Powell <clearance.clarence@gmail.com>
2019-08-19 12:14:49 -0500

    update logo graphic (#40)

--- payments/urls.py
+++ payments/urls.py
@@ -5,4 +5,10 @@ from payments.views import *
 urlpatterns = [
 
     url(r'^payments/stripe_webhook$', stripe_webhook_view, name='stripe_webhook'),
+
+    # url(r'^payments/subscription_completed_webhook$', subscription_completed_webhook_view, name='subscription_completed_webhook'),
+    # url(r'^payments/subscription_canceled_webhook$', subscription_canceled_webhook_view, name='subscription_canceled_webhook'),
+    #
+    url(r'^payments/success$', success_view, name='payment_success'),
+    url(r'^payments/canceled$', canceled_view, name='payment_canceled'),
     ]
