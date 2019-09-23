792cfc148f15cb092374fccd331e2bcae5f83f88
Blake Powell <clearance.clarence@gmail.com>
2019-09-22 21:18:58 -0400

    reodered signals

--- flights/apps.py
+++ flights/apps.py
@@ -5,15 +5,14 @@ class FlightsConfig(AppConfig):
 
     def ready(self):
         import flights.signal_stat
-        
-        import flights.signal_extra
+
+
         import flights.signal_total
-
-        import flights.signal_route_data
-
+        import flights.signal_import
         import flights.signal_error_flight
         import flights.signal_error_aircraft
         import flights.signal_error_tailnumber
-        import flights.signal_import
+        import flights.signal_route_data
+        import flights.signal_extra
 
         import accounts.signal_profile
