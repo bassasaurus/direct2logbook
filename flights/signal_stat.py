b95fab8d77d2accb0cef5878cbbf170f3db9672d
Blake Powell <clearance.clarence@gmail.com>
2019-09-22 23:34:53 -0400

    imported to aircraft stats complete

--- flights/signal_stat.py
+++ flights/signal_stat.py
@@ -5,6 +5,7 @@ from django.db.models import Sum, Q
 from django.dispatch import receiver
 import datetime
 from django.core.exceptions import ObjectDoesNotExist
+from flights.queryset_helpers import *
 
 @receiver(post_delete, sender=Flight)
 def no_flight_stat_delete(sender, instance, **kwargs):
@@ -23,6 +24,7 @@ def no_flight_stat_delete(sender, instance, **kwargs):
         pass
 
 @receiver(pre_delete, sender=Aircraft)
+@receiver(pre_delete, sender=Imported)
 def no_aircraft_stat_delete(sender, instance, **kwargs):
 
     user = instance.user
@@ -34,40 +36,80 @@ def no_aircraft_stat_delete(sender, instance, **kwargs):
     except ObjectDoesNotExist:
         pass
 
+@receiver(post_save, sender=Imported)
 @receiver(post_save, sender=Flight)
+@receiver(pre_delete, sender=Imported)
 @receiver(pre_delete, sender=Flight)
 def stat_update(sender, instance, **kwargs):
 
-    user = instance.user
+    stat = Stat.objects.get_or_create(user=instance.user, aircraft_type=instance.aircraft_type)
+    stat=stat[0]
+    flight = Flight.objects.filter(user=instance.user).filter(aircraft_type=instance.aircraft_type)
+
+    imported = instance
+
+    stat.total_time = avoid_none(flight, 'duration') + imported.total_time
+
+    stat.pilot_in_command = avoid_none(flight.filter(pilot_in_command=True), 'duration') + imported.pilot_in_command
+
+    stat.second_in_command = avoid_none(flight.filter(second_in_command=True), 'duration') + imported.second_in_command
+
+    stat.cross_country = avoid_none(flight.filter(cross_country=True), 'duration') + imported.cross_country
+
+    stat.instructor = avoid_none(flight.filter(instructor=True), 'duration') + imported.instructor
+
+    stat.dual = avoid_none(flight.filter(dual=True), 'duration') + imported.dual
+
+    stat.solo = avoid_none(flight.filter(solo=True), 'duration') + imported.solo
+
+    stat.instrument = avoid_none(flight, 'instrument') + imported.instrument
+
+    stat.simulated_instrument = avoid_none(flight, 'simulated_instrument') + imported.simulated_instrument
+
+    stat.simulator = avoid_none(flight.filter(simulator=True), 'duration') + imported.simulator
+
+    stat.night = avoid_none(flight, 'night') + imported.night
+
+    stat.landings_day = avoid_none(flight, 'landings_day') + imported.landings_day
+
+    stat.landings_night = avoid_none(flight, 'landings_night') + imported.landings_night
+
+    stat.landings_stat = stat.landings_day + stat.landings_night + imported.landings_day + imported.landings_night
 
     try:
-        stat = Stat.objects.get_or_create(user=user, aircraft_type = instance.aircraft_type)
-
-        aircraft_type = instance.aircraft_type
-
-        Stat.objects.filter(user=user).filter(aircraft_type = aircraft_type).update(
-            aircraft_type = str(aircraft_type),
-            total_time = querys.aircraft_total_time(aircraft_type),
-            pilot_in_command = querys.pilot_in_command_total(aircraft_type),
-            second_in_command = querys.second_in_command_total(aircraft_type),
-            cross_country = querys.cross_country_total(aircraft_type),
-            instructor = querys.instructor_total(aircraft_type),
-            dual = querys.dual_total(aircraft_type),
-            solo = querys.solo_total(aircraft_type),
-            instrument = querys.instrument_total(aircraft_type),
-            simulated_instrument = querys.simulated_instrument_total(aircraft_type),
-            simulator = querys.simulator_total(aircraft_type),
-            night = querys.night_total(aircraft_type),
-            landings_day = querys.landings_day_total(aircraft_type),
-            landings_night = querys.landings_night_total(aircraft_type),
-            last_flown = querys.last_flown(aircraft_type),
-            last_30 = querys.last_30(aircraft_type),
-            last_60 = querys.last_60(aircraft_type),
-            last_90 = querys.last_90(aircraft_type),
-            last_180 = querys.last_yr(aircraft_type),
-            last_yr = querys.last_yr(aircraft_type),
-            last_2yr = querys.last_2yr(aircraft_type),
-            ytd = querys.ytd(aircraft_type),
-            )
+        last_flown = flight.latest('date')
+        stat.last_flown = last_flown
     except ObjectDoesNotExist:
-        pass
+        stat.last_flown = imported.last_flown
+
+    today = datetime.date.today()
+
+    last_30 = today - datetime.timedelta(days=30)
+    last_30 = flight.filter(date__lte=today,date__gte=last_30)
+    stat.last_30 = avoid_none(last_30, 'duration') + imported.last_30
+
+    last_60 = today - datetime.timedelta(days=60)
+    last_60 = flight.filter(date__lte=today,date__gte=last_60)
+    stat.last_60 = avoid_none(last_60, 'duration') + imported.last_60
+
+    last_90 = today - datetime.timedelta(days=90)
+    last_90 = flight.filter(date__lte=today,date__gte=last_90)
+    stat.last_90 = avoid_none(last_90, 'duration') + imported.last_90
+
+    last_180 = today - datetime.timedelta(days=180)
+    last_180 = flight.filter(date__lte=today,date__gte=last_180)
+    stat.last_180 = avoid_none(last_180, 'duration') + imported.last_180
+
+    last_yr = today - datetime.timedelta(days=365)
+    last_yr = flight.filter(date__lte=today,date__gte=last_yr)
+    stat.last_yr = avoid_none(last_yr, 'duration') + imported.last_yr
+
+    last_2yr = today - datetime.timedelta(days=730)
+    last_2yr = flight.filter(date__lte=today,date__gte=last_2yr)
+    stat.last_2yr = avoid_none(last_2yr, 'duration') + imported.last_2yr
+
+    ytd = datetime.date(today.year, 1, 1)
+    ytd = flight.filter(date__lte=today,date__gte=ytd)
+    stat.ytd = avoid_none(ytd, 'duration') + imported.ytd
+
+    stat.save()
