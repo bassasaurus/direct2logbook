008f6fe716c51646ec4603933b0544e5ed1d8828
blake <clearance.clarence@gmail.com>
2019-10-15 16:17:39 -0700

    date parse

--- csv_app/csvimport.py
+++ csv_app/csvimport.py
@@ -3,6 +3,7 @@ import csv
 import io
 import datetime
 from flights.models import Flight, Aircraft, Approach, TailNumber
+from dateutil.parser import parse
 
 
 # converts values, if they exist, to boolean objects
@@ -21,7 +22,7 @@ def convertBool(row_id):
 
 def addFKAircraft(row_id, user):
     try:
-        obj = Aircraft.objects.filter(user=user).get(aircraft_type=row_id)
+        obj = Aircraft.objects.filter(user=user).get(aircraft_type=str(row_id))
     except Aircraft.DoesNotExist:
         obj = Aircraft(user=user, aircraft_type=row_id)
         obj.save()
@@ -38,6 +39,7 @@ def addFKTailnumber(row_id, user):
 
 
 def import_csv(request):
+    user = request.user
 
     file = request.FILES['file']
 
@@ -45,47 +47,37 @@ def import_csv(request):
 
     io_string = io.StringIO(decoded_file)
 
-    for line in csv.reader(io_string, delimiter=','):
-        print(line)
+    next(io_string) #skips header row
 
-    # next(reader)  # skips header
+    for row in csv.reader(io_string, delimiter=','):
 
-    # for row in reader:  # iterates rows
-    #     print(row)
-        # # date to python datetime object
-        # date = datetime.datetime.strptime(row[0], '%m/%d/%Y').date()
-        # row[0] = date
-        #
-        # # makes any empty entry default to 0
-        # for n, i in enumerate(row):
-        #     if i == '':
-        #         row[n] = 0
-        #
-        # aircraft = addFKAircraft(user, row[1])
-        #
-        # tailnumber = addFKTailnumber(user, row[2])
-        #
-        # flight = Flight(
-        #     user=user,
-        #     date=row[0],
-        #     aircraft=aircraft,
-        #     registration=tailnumber,
-        #     route=row[3],
-        #     duration=row[4],
-        #     pilot_in_command=convertBool(row[5]),
-        #     second_in_command=convertBool(row[6]),
-        #     cross_country=convertBool(row[7]),
-        #     night=row[8],
-        #     instrument=row[9],
-        #     landings_day=int(row[10]),
-        #     landings_night=int(row[11]),
-        #     simulated_instrument=row[12],
-        #     instructor=convertBool(row[13]),
-        #     dual=convertBool(row[14]),
-        #     solo=convertBool(row[15]),
-        #     simulator=convertBool(row[16]),
-        #     remarks=row[17],
-        # )
-        # print(flight.date, flight.route)
+        # makes any empty entry default to 0
+        for n, i in enumerate(row):
+            if i == '':
+                row[n] = 0
+
+        flight = Flight(
+            user=user,
+            date=parse(row[0]),
+            aircraft=addFKAircraft(user, row[1]),
+            registration=addFKTailnumber(user, row[2]),
+            route=row[3],
+            duration=row[4],
+            pilot_in_command=convertBool(row[5]),
+            second_in_command=convertBool(row[6]),
+            cross_country=convertBool(row[7]),
+            night=row[8],
+            instrument=row[9],
+            landings_day=int(row[10]),
+            landings_night=int(row[11]),
+            simulated_instrument=row[12],
+            instructor=convertBool(row[13]),
+            dual=convertBool(row[14]),
+            solo=convertBool(row[15]),
+            simulator=convertBool(row[16]),
+            remarks=row[17],
+        )
+
+        print(flight.date, flight.route)
 
         # flight.save()
