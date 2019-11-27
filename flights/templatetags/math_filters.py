4ac8a6754bb565d7440dc5873435d14fb231bece
bassasaurus <clearance.clarence@gmail.com>
2019-11-27 17:59:22 -0500

    re-enabled addition template filter

--- flights/templatetags/math_filters.py
+++ flights/templatetags/math_filters.py
@@ -5,5 +5,5 @@ register = template.Library()
 
 @register.simple_tag(name='addition')
 def addition(*args):
-    return None
-    # return round(sum(list(args)), 1)
+    # return None
+    return round(sum(list(args)), 1)
