aa1049fbce49a61404af9e766f34d50e0cc9b692
bassasaurus <clearance.clarence@gmail.com>
2019-12-10 16:03:57 -0600

    intermediate

--- accounts/update_profiles.py
+++ accounts/update_profiles.py
@@ -19,14 +19,15 @@ def update_profiles():
             pass
         else:
             user = User.objects.get(pk=profile.user.pk)
-            name = '{} {}'.format(user.first_name, user.last_name )
+            name = '{} {}'.format(user.first_name, user.last_name)
 
             stripe.api_key = config('STRIPE_TEST_SECRET_KEY')
 
             now = datetime.now()
             trial_period = timedelta(days=14)
             end_date = now + trial_period
-            timestamp = round(end_date.replace(tzinfo=timezone.utc).timestamp())
+            timestamp = round(end_date.replace(
+                tzinfo=timezone.utc).timestamp())
 
             customer_response = stripe.Customer.create(
                 description=name,
@@ -39,25 +40,26 @@ def update_profiles():
         else:
             subscription_response = stripe.Subscription.create(
                 customer=customer_response.id,
-                collection_method = "send_invoice",
-                days_until_due = 14,
+                collection_method="send_invoice",
+                days_until_due=14,
                 cancel_at_period_end=True,
                 items=[
                     {
                         "plan": "plan_FZhtfxftM44uHz",
                     },
                 ],
-                trial_end = timestamp,
+                trial_end=timestamp,
             )
 
             timestamp = subscription_response.trial_end
 
-            profile.customer_id=customer_response.id
-            profile.subscription_id=subscription_response.id
-            profile.active=False
-            profile.trial=True
-            profile.free_access=False
-            profile.trial_expiring=False
-            profile.end_date=datetime.fromtimestamp(timestamp)
+            profile.customer_id = customer_response.id
+            profile.subscription_id = subscription_response.id
+            profile.active = False
+            profile.trial = True
+            profile.free_access = False
+            profile.trial_expiring = False
+            profile.end_date = datetime.fromtimestamp(timestamp)
 
-        profile.save(update_fields=['customer_id', 'subscription_id', 'active', 'trial', 'free_access', 'trial_expiring', 'end_date'])
+        profile.save(update_fields=['customer_id', 'subscription_id',
+                                    'active', 'trial', 'free_access', 'trial_expiring', 'end_date'])
