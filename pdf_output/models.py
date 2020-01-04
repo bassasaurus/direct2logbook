b683a9786ee1f1a4c97617722d266d084fa2e037
bassasaurus <clearance.clarence@gmail.com>
2020-01-04 18:22:38 -0500

    removed single object validation

--- pdf_output/models.py
+++ pdf_output/models.py
@@ -13,10 +13,3 @@ def user_directory_path(instance, filename):
 class Signature(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     signature = models.ImageField(upload_to=user_directory_path)
-
-    def save(self, *args, **kwargs):
-        if not self.pk and Signature.objects.exists():
-        # if you'll not check for self.pk
-        # then error will also raised in update of exists model
-            raise ValidationError('There can be only one Signature')
-        return super(Signature, self).save(*args, **kwargs)
