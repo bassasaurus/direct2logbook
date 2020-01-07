23733b7f49d38553d413662c4729590219d0c538
bassasaurus <clearance.clarence@gmail.com>
2020-01-07 13:37:06 -0500

    media url

--- logbook/settings.py
+++ logbook/settings.py
@@ -236,6 +236,9 @@ AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
 AWS_DEFAULT_ACL = None
 AWS_S3_FILE_OVERWRITE = False
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAdminUser',
