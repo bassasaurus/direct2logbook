2b54984953da80c878585965d669b17cf94000d6
bassasaurus <clearance.clarence@gmail.com>
2020-01-08 15:20:19 -0800

    media path

--- pdf_output/models.py
+++ pdf_output/models.py
@@ -7,7 +7,7 @@ from django.utils import timezone
 
 def user_directory_path(instance, filename):
     # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
-    return 'user_{0}/{1}'.format(instance.user.id, filename)
+    return 'media/user_{0}/{1}'.format(instance.user.id, filename)
 
 
 class Signature(models.Model):
--- pdf_output/pdf_generate_task.py
+++ pdf_output/pdf_generate_task.py
@@ -83,7 +83,7 @@ def pdf_generate(user_pk):
         if Signature.objects.filter(user=user).exists():
             sig = Signature.objects.get(user=user)
             signature = sig.signature
-            signature_path = settings.MEDIA_URL + '/' + str(signature)
+            signature_path = settings.MEDIA_URL + str(signature)
             canvas.drawImage(str(signature_path), 240,
                              50, width=100, height=40)
         else:
