13bccad690c5b79b5438b2a9549beb4d266bade6
bassasaurus <clearance.clarence@gmail.com>
2020-01-08 17:23:00 -0800

    back to the drawng board

--- pdf_output/pdf_generate_task.py
+++ pdf_output/pdf_generate_task.py
@@ -84,8 +84,7 @@ def pdf_generate(user_pk):
         if Signature.objects.filter(user=user).exists():
             sig = Signature.objects.get(user=user)
             signature = sig.signature
-            signature_path = str(signature)
-            
+            signature_path = config('MEDIA_URL') + str(signature)
             canvas.drawImage(str(signature_path), 240,
                              50, width=100, height=40)
         else:
