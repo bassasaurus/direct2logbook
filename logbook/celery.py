da4a00b861174c3e984a3e36e8189ef3a468b889
bassasaurus <clearance.clarence@gmail.com>
2020-01-15 22:42:18 -0500

    namespace

--- logbook/celery.py
+++ logbook/celery.py
@@ -19,7 +19,7 @@ app = Celery('logbook', broker=config('CLOUDAMQP_URL'))
 # the configuration object to child processes.
 # - namespace='CELERY' means all celery-related configuration keys
 #   should have a `CELERY_` prefix.
-app.config_from_object('django.conf:settings', namespace='CELERY')
+app.config_from_object('django.conf:settings', namespace='None')
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
