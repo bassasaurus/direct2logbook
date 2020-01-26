292a309d57ec193954c37574f1a08ada02b4f2e0
bassasaurus <clearance.clarence@gmail.com>
2020-01-25 19:41:25 -0600

    undo cast=bool

--- logbook/settings.py
+++ logbook/settings.py
@@ -289,5 +289,5 @@ DEFAULT_FROM_EMAIL = "no-reply@direct2logbook.com"
 
 CSRF_USE_SESSIONS = True
 
-if os.environ.get('DJANGO_DEVELOPMENT_SETTINGS', cast=bool):
+if os.environ.get('DJANGO_DEVELOPMENT_SETTINGS'):
     from .development import *
