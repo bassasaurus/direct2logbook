1b552776c9b44d47acfe746e845a9e55a9e7d4aa
bassasaurus <clearance.clarence@gmail.com>
2020-02-04 13:15:00 -0500

    profile admin dsplay

--- profile/admin.py
+++ profile/admin.py
@@ -3,7 +3,20 @@ from .models import Profile
 
 
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'pk', 'company')  # add signature
+    list_display = (
+            'user',
+            'pk',
+            'customer_id',
+            'subscription_id',
+            'trial',
+            'trial_expiring',
+            'active',
+            'monthly',
+            'yearly',
+            'end_date',
+            'canceled',
+            'expired',
+            'company', )  # add signature
     empty_value_display = ''
 
 
