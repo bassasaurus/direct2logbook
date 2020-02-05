278a88b11db692712a41a2743a6d1e85bb2cf0a1
bassasaurus <clearance.clarence@gmail.com>
2020-02-04 20:11:31 -0500

    admin filter by user

--- flights/admin.py
+++ flights/admin.py
@@ -52,16 +52,20 @@ class FlightAdmin(admin.ModelAdmin):
      'duplicate_error', 'aircraft_type_error', 'registration_error', 'crew_error')
     empty_value_display = ''
 
+    list_filter = ('user',)
+
 class AircraftAdmin(admin.ModelAdmin):
     list_display = ('aircraft_type', 'user', 'pk', 'turbine', 'piston', 'requires_type',
     'tailwheel', 'compleks', 'high_performance', 'aircraft_category', 'aircraft_class', 'image')
 
-    search_fields = ['user']
+    list_filter = ('user',)
 
 class TailnumberAdmin(admin.ModelAdmin):
     list_display = ('registration', 'user', 'pk', 'aircraft', 'is_121', 'is_135', 'is_91')
     search_fields = ('registration',)
     empty_value_display = ''
+
+    list_filter = ('user',)
 
 class MapDataAdmin(admin.ModelAdmin):
     list_display = ('icao', 'iata', 'name', 'city', 'state', 'country', 'latitude', 'longitude', 'elevation')
@@ -72,24 +76,36 @@ class TotalAdmin(admin.ModelAdmin):
     list_display = ('user', 'pk', 'total', 'total_time', 'pilot_in_command', 'second_in_command', 'cross_country', 'instructor', 'dual', 'solo', 'instrument', 'night', 'simulated_instrument', 'simulator', 'landings_day', 'landings_night', 'landings_total')
     empty_value_display = ''
 
+    list_filter = ('user',)
+
 class StatAdmin(admin.ModelAdmin):
     list_display = ('user', 'pk', 'aircraft_type', 'total_time')
     empty_value_display = ''
 
+    list_filter = ('user',)
+
 class ApproachAdmin(admin.ModelAdmin):
     list_display = ('flight_object', 'approach_type', 'number')
+
 
 class HoldingAdmin(admin.ModelAdmin):
     list_display = ('flight_object', 'hold')
 
+
 class RegAdmin(admin.ModelAdmin):
     list_display = ('user', 'reg_type', 'pilot_in_command', 'second_in_command')
+
+    list_filter = ('user',)
 
 class WeightAdmin(admin.ModelAdmin):
     list_display = ('user', 'weight', 'total')
 
+    list_filter = ('user',)
+
 class PowerAdmin(admin.ModelAdmin):
     list_display = ('user', 'role', 'turbine', 'piston')
+
+    list_filter = ('user',)
 
 # admin.site.unregister(User)
 # admin.site.register(User, UserAdmin)
