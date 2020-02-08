bdfd389e8705c986f1814d489ea81670c158c0f4
bassasaurus <clearance.clarence@gmail.com>
2020-02-07 19:31:55 -0500

    prod paths

--- logbook/settings/dev.py
+++ logbook/settings/dev.py
@@ -1,5 +1,7 @@
 from .base import *
 
+print(__file__)
+
 DEBUG = True
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
--- logbook/settings/prod.py
+++ logbook/settings/prod.py
@@ -1,10 +1,15 @@
 from .base import *
+import os
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 from sentry_sdk.integrations.celery import CeleryIntegration
 from decouple import config
 
 DEBUG = False
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+DJANGO_ROOT = dirname(dirname(abspath(__file__)))
 
 sentry_sdk.init(
     dsn="https://e3f79fa21e484fe6b58a2e227a5bbce5@sentry.io/1515848",
