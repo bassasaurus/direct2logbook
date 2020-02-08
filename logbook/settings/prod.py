3b153d026fe075320a79f43c348ed4ce267f2ed2
bassasaurus <clearance.clarence@gmail.com>
2020-02-07 19:47:48 -0500

    restored original paths

--- logbook/settings/prod.py
+++ logbook/settings/prod.py
@@ -7,10 +7,6 @@ from decouple import config
 
 DEBUG = False
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-DJANGO_ROOT = dirname(dirname(abspath(__file__)))
-
 sentry_sdk.init(
     dsn="https://e3f79fa21e484fe6b58a2e227a5bbce5@sentry.io/1515848",
     integrations=[DjangoIntegration(), CeleryIntegration()]
