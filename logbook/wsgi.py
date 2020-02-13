9bba7d47718d6b5a600953f9b3ce4f599acf077c
Blake Powell <clearance.clarence@gmail.com>
2020-02-12 18:24:37 -0600

    settings.prod for default (#111)

--- logbook/wsgi.py
+++ logbook/wsgi.py
@@ -9,8 +9,7 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
-from decouple import config
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", config('PATH_TO_SETTINGS'))
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", 'logbook.settings.prod')
 
 application = get_wsgi_application()
