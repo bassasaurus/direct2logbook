a8a2b9eed03cf9d4098fb0a7f366d81a12787359
Blake Powell <clearance.clarence@gmail.com>
2020-02-24 18:02:36 -0600

    simple celery (#121)

--- logbook/celery_config.py
+++ logbook/celery_config.py
@@ -1,6 +1,17 @@
-from decouple import config
 
-broker_url = config('CLOUDAMQP_URL')
+
+
+broker_transport_options = {
+        'data_folder_in': 'broker/out',
+        'data_folder_out': 'broker/out',
+        'data_folder_processed': 'broker/processed'}
+
+
+result_persistent = False
+task_serializer = 'json'
+result_serializer = 'json'
+accept_content = ['json']
+
 broker_pool_limit = 1  # Will decrease connection usage
 broker_heartbeat = None  # We're using TCP keep-alive instead
 broker_connection_timeout = 30  # May require a long timeout due to Linux DNS timeouts etc
