418207f77f2879fbbd341fbca5ddf5d7696df45b
bassasaurus <clearance.clarence@gmail.com>
2020-05-19 13:53:29 -0500

    login view response

--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import url, include
-from . import views
+from .views import LogoutUserAPIView, UserViewSet, FlightViewSet, AircraftViewSet, TailNumberViewSet
 from rest_framework import routers
 from rest_framework.authtoken.views import obtain_auth_token
 
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'flights', views.FlightViewSet)
-router.register(r'aircraft', views.AircraftViewSet)
-router.register(r'tailnumber', views.TailNumberViewSet)
+router.register(r'users', UserViewSet)
+router.register(r'flights', FlightViewSet)
+router.register(r'aircraft', AircraftViewSet)
+router.register(r'tailnumber', TailNumberViewSet)
 
 urlpatterns = [
     url(r'^',
