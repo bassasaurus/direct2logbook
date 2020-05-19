de9e674e5a2a94f07a1c4d9b480e99b4b0776486
bassasaurus <clearance.clarence@gmail.com>
2020-05-19 13:34:31 -0500

    removed unused serializers and urls

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
-from flights.models import *
+from flights.models import Flight, Aircraft, TailNumber
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -9,42 +9,19 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
         fields = ('url', 'username', 'email', 'groups')
 
 
-class GroupSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Group
-        fields = ('url', 'name')
-
-
 class FlightSerializer(serializers.HyperlinkedModelSerializer):
-	class Meta:
-		model = Flight
-		fields = '__all__'
+    class Meta:
+        model = Flight
+        fields = '__all__'
 
 
 class AircraftSerializer(serializers.HyperlinkedModelSerializer):
-	class Meta:
-		model = Aircraft
-		fields = '__all__'
+    class Meta:
+        model = Aircraft
+        fields = '__all__'
+
 
 class TailNumberSerializer(serializers.HyperlinkedModelSerializer):
-	class Meta:
-		model = TailNumber
-		fields = '__all__'
-
-
-class AircraftCategorySerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        model = AircraftCategory
-        fields = '__all__'
-
-
-class AircraftClassSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = AircraftClass
-        fields = '__all__'
-
-
-class ApproachSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Approach
+        model = TailNumber
         fields = '__all__'
--- api/urls.py
+++ api/urls.py
@@ -4,13 +4,9 @@ from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
 router.register(r'flights', views.FlightViewSet)
 router.register(r'aircraft', views.AircraftViewSet)
 router.register(r'tailnumber', views.TailNumberViewSet)
-router.register(r'aircraft_category', views.AircraftCategoryViewSet)
-router.register(r'aircraft_class', views.AircraftClassViewSet)
-router.register(r'approaches', views.ApproachViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
