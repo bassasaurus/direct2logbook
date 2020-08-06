0667443afd6a5451fdcbf6554186f8e32d74bb00
Blake Powell <clearance.clarence@gmail.com>
2020-08-06 10:18:53 -0500

    Envars (#155)
    * removed dotenv
    
    * updated
    
    * removed dotenv

--- logbook/celery_config.py
+++ logbook/celery_config.py
@@ -1,9 +1,6 @@
-from dotenv import load_dotenv
-import os
+from decouple import config
 
-load_dotenv(verbose=True)
-
-broker_url = os.getenv('CLOUDAMQP_URL')
+broker_url = config('CLOUDAMQP_URL')
 broker_pool_limit = 1  # Will decrease connection usage
 broker_heartbeat = None  # We're using TCP keep-alive instead
 broker_connection_timeout = 30  # May require a long timeout due to Linux DNS timeouts etc
