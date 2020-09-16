f06f577c4cbaa161dd439bd2a401f1a85eb999c3
bassasaurus <clearance.clarence@gmail.com>
2020-09-16 16:53:48 -0500

    captcha widget renders in signup page

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,10 +2,25 @@ from django import forms
 from django.contrib.auth.models import User
 from allauth.account.forms import SignupForm
 
+from captcha.fields import ReCaptchaField
+from captcha.fields import ReCaptchaV2Checkbox
+from decouple import config
+
 
 class CustomSignupForm(SignupForm):
     first_name = forms.CharField(max_length=30, label='First Name')
     last_name = forms.CharField(max_length=30, label='Last Name')
+
+    captcha = ReCaptchaField(
+        public_key=config('RECAPTCHA_PUBLIC_KEY'),
+        private_key=config('RECAPTCHA_PRIVATE_KEY'),
+        
+        widget=ReCaptchaV2Checkbox(
+            attrs={
+                'required_score': 0.0,
+            }
+        )
+    )
 
     def signup(self, request, user):
         user.first_name = self.cleaned_data['first_name']
