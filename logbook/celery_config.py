a0026aadf1511b9fb7d55689f87c74a0feeee2e1
bassasaurus <clearance.clarence@gmail.com>
2020-09-21 17:20:35 -0500

    BROKER_URL

--- logbook/celery_config.py
+++ logbook/celery_config.py
@@ -1,6 +1,6 @@
 from decouple import config
 
-broker_url = config('CLOUDAMQP_URL')
+broker_url = config('BROKER_URL')
 broker_pool_limit = 1  # Will decrease connection usage
 broker_heartbeat = None  # We're using TCP keep-alive instead
 broker_connection_timeout = 30  # May require a long timeout due to Linux DNS timeouts etc
