0b4748cfb72d4f0efd716333a6ae742b85b14d2a
bassasaurus <clearance.clarence@gmail.com>
2020-09-21 14:02:14 -0500

    csv task registred

--- csv_app/csv_import.py
+++ csv_app/csv_import.py
@@ -3,9 +3,9 @@ import io
 from flights.models import Flight, Aircraft, TailNumber, MapData, Approach
 from dateutil.parser import parse
 import re
-from logbook.celery import app
 from .formatters import check_date, check_float, format_route, check_text, convertBool, assign_ils
 from django.core.mail import EmailMessage
+from huey.contrib.djhuey import task
 
 
 def save_route_data(user, route):
@@ -58,7 +58,7 @@ def email_confirmation(request):
     email.send()
 
 
-@app.task
+@task()
 def csv_import(request, file):
 
     user = request.user
