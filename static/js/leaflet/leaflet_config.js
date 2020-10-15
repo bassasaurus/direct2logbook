cb190e7dcf3d7bba01d2b6fe0bca0a56a6972ce0
bassasaurus <clearance.clarence@gmail.com>
2020-10-15 18:27:03 -0500

    openstreet map

--- static/js/leaflet/leaflet_config.js
+++ static/js/leaflet/leaflet_config.js
@@ -10,12 +10,9 @@ var mymap = L.map('mapid', {
   loadingControl: true,
 });
 
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-  attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-  maxZoom: 15,
-  id: 'mapbox.streets',
-  accessToken: 'pk.eyJ1IjoicGlnYXJrbGUiLCJhIjoiY2o3bGZ3Z2txMnB0cDJxbHhndjJkbnRvciJ9.IytedDwzW9skTCe_7d_gdQ'
-  }).addTo(mymap);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(mymap);
 
 // reload on back button
 if(!!window.performance && window.performance.navigation.type == 2){
