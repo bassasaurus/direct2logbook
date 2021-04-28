329743a36a333ef0a18ec86d1f9d2344b40e4941
bassasaurus <clearance.clarence@gmail.com>
2021-04-28 11:01:16 -0500

    date format for serializer

--- logbook/settings.py
+++ logbook/settings.py
@@ -321,5 +321,6 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 100
+    'PAGE_SIZE': 100,
+    'DATE_FORMAT': '%d-%m-%Y'
 }
