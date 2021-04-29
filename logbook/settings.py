7057c0d6487dc17afa6b9080db4e9e8b00d22e3e
bassasaurus <clearance.clarence@gmail.com>
2021-04-29 15:02:04 -0500

    date format

--- logbook/settings.py
+++ logbook/settings.py
@@ -322,5 +322,5 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 100,
-    'DATE_FORMAT': '%d-%m-%Y'
+    'DATE_FORMAT': '%m-%d-%Y'
 }
