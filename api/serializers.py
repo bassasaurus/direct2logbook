cc339613d7c7b66df6d43d260f5e37c8531c950c
bassasaurus <clearance.clarence@gmail.com>
2021-09-21 12:22:57 -0500

    create instance complete

--- api/serializers.py
+++ api/serializers.py
@@ -37,42 +37,28 @@ class HoldingSerializer(serializers.ModelSerializer):
 
 class FlightSerializer(serializers.ModelSerializer):
 
-    def get_approaches(self, obj):
-        approach_queryset = Approach.objects.filter(flight_object=obj.pk)
-        return ApproachSerializer(approach_queryset, many=True).data
-    
+    class Meta:
+        model = Flight
+        fields = ['id', 'user', 'date', 'aircraft_type', 'registration', 'route', 'duration', 'landings_day', 'landings_night', 
+                    'night', 'instrument', 'cross_country', 'second_in_command', 'pilot_in_command', 'simulated_instrument', 
+                    'instructor', 'dual', 'remarks', 'solo', 'route_data', 'app_markers', 'app_polylines','approaches']
+
     aircraft_type = serializers.StringRelatedField()
     registration = serializers.StringRelatedField()
     approaches = SerializerMethodField(source='get_approaches')
 
-    
-    class Meta:
-        model = Flight
-        fields = ['id',
-                    'date', 
-                    'aircraft_type', 
-                    'registration', 
-                    'route', 
-                    'duration', 
-                    'landings_day', 
-                    'landings_night', 
-                    'night', 
-                    'instrument', 
-                    'cross_country', 
-                    'second_in_command', 
-                    'pilot_in_command', 
-                    'simulated_instrument', 
-                    'instructor', 
-                    'dual', 
-                    'remarks', 
-                    'solo', 
-                    'route_data', 
-                    'app_markers', 
-                    'app_polylines',
-                    
-                    'approaches']
+    def get_approaches(self, obj):
+        approach_queryset = Approach.objects.filter(flight_object=obj.pk)
+        return ApproachSerializer(approach_queryset, many=True).data
 
-                    
+    def create(self, validated_data):
+
+        validated_data['aircraft_type'] = Aircraft.objects.get(pk=self.initial_data.get('aircraft_type'))
+        validated_data['registration'] = TailNumber.objects.get(pk=self.initial_data.get('registration'))
+
+        return Flight.objects.create(**validated_data)
+
+
 class TailNumberSerializer(serializers.ModelSerializer):
     class Meta:
         model = TailNumber
--- api/views.py
+++ api/views.py
@@ -59,12 +59,12 @@ class FlightViewSet(viewsets.ModelViewSet, CreateModelMixin):
 
     def create(self, request, *args, **kwargs):
 
+        request.data['user'] = self.request.user.pk
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
 
-        print(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
     def perform_create(self, serializer):
