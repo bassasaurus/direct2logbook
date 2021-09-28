a1a7325dec6f5ca05dfa58121eb7653a4e517092
bassasaurus <clearance.clarence@gmail.com>
2021-09-27 19:41:37 -0500

    approach_list validated

--- api/serializers.py
+++ api/serializers.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User, Group
+from django.core.validators import validate_slug
 from rest_framework import serializers
 from rest_framework.fields import CharField, SerializerMethodField
 
@@ -50,8 +51,40 @@ class FlightSerializer(serializers.ModelSerializer):
 
     landings_day = serializers.IntegerField(min_value=0, max_value=20, required=False, allow_null=True)
     landings_night = serializers.IntegerField(min_value=0, max_value=20, required=False, allow_null=True)
-    instrument = serializers.IntegerField(min_value=0, max_value=20, required=False, allow_null=True)
-    simulated_instrument = serializers.IntegerField(min_value=0, max_value=20, required=False, allow_null=True)
+    instrument = serializers.FloatField(required=False, allow_null=True)
+    simulated_instrument = serializers.FloatField(required=False, allow_null=True)
+
+    def to_internal_value(self, data):
+        if data.get('landings_day') == '':
+            del data['landings_day']
+
+        if data.get('landings_night') == '':
+            del data['landings_night']
+
+        if data.get('instrument') == '':
+            del data['instrument']
+
+        if data.get('simulated_instrument') == '':
+           del data['simulated_instrument']
+        
+        approach_list = data.get('approaches')
+
+        validated_list = []
+
+        for d in approach_list:
+            if d.get('number') == '':
+                pass
+            else:
+                validated_list.append(d)
+
+        data['approaches'] = validated_list
+            
+        print(data.get('approaches'))
+        
+
+        
+
+        return super(FlightSerializer, self).to_internal_value(data)
 
     def get_approaches(self, obj):
         approach_queryset = Approach.objects.filter(flight_object=obj.pk)
@@ -63,11 +96,9 @@ class FlightSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
 
-
         validated_data['aircraft_type'] = Aircraft.objects.get(pk=self.initial_data.get('aircraft_type'))
         validated_data['registration'] = TailNumber.objects.get(pk=self.initial_data.get('registration'))
 
-        
         approaches = self.initial_data.get('approaches')
 
         flight = Flight(**validated_data)
