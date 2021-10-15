9fb73d5610a7d1e637d75999988cc4587019b8b9
bassasaurus <clearance.clarence@gmail.com>
2021-10-15 19:19:50 -0400

    DRF date format

--- logbook/settings.py
+++ logbook/settings.py
@@ -324,5 +324,6 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 100,
+    'DATE_FORMAT': '%m/%d/%Y',
     'DATE_INPUT_FORMATS': ['%m/%d/%Y']
 }
