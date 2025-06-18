81fe527b8ef4f0f982d7d46fc2e3c4364a8f9f60
bassasaurus <clearance.clarence@gmail.com>
2025-06-18 14:09:05 -0500

    packages updates

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,25 +1,25 @@
-from django.urls import path
+from django.urls import re_path, re_path
 from .views import UserUpdateView, EmailView, ConnectionsView, PasswordSetView, PasswordChangeView, PasswordResetView, PasswordResetDoneView, PasswordResetFromKeyView, PasswordResetFromKeyDoneView
 
 
 urlpatterns = [
-    path(r'^accounts/user/update/(?P<pk>\d+)/$',
-         UserUpdateView.as_view(), name='user_update'),
+    re_path(r'^accounts/user/update/(?P<pk>\d+)/$',
+            UserUpdateView.as_view(), name='user_update'),
     # override allauth urls
-    path(r'^accounts/email/$', EmailView.as_view(), name='account_email'),
-    path(r'^accounts/social/connections/$',
-         ConnectionsView.as_view(), name='socialaccount_connections'),
-    path(r'^accounts/password/set/$', PasswordSetView.as_view(),
-         name='account_set_password'),
-    path(r'^accounts/password/change/$', PasswordChangeView.as_view(),
-         name='account_change_password'),
-    path(r'^accounts/password/reset/$', PasswordResetView.as_view(),
-         name='account_change_password'),
-    path(r'^accounts/password/reset/done/$',
-         PasswordResetDoneView.as_view(), name='account_password_reset_done'),
-    path(r"^accounts/password/reset/key/(?P<uidb36>[0-9A-Za-z]+)-(?P<key>.+)/$",
-         PasswordResetFromKeyView.as_view(),
-         name="account_reset_password_from_key"),
-    path(r"^password/reset/key/done/$", PasswordResetFromKeyDoneView.as_view(),
-         name="account_reset_password_from_key_done"),
+    re_path(r'^accounts/email/$', EmailView.as_view(), name='account_email'),
+    re_path(r'^accounts/social/connections/$',
+            ConnectionsView.as_view(), name='socialaccount_connections'),
+    re_path(r'^accounts/password/set/$', PasswordSetView.as_view(),
+            name='account_set_password'),
+    re_path(r'^accounts/password/change/$', PasswordChangeView.as_view(),
+            name='account_change_password'),
+    re_path(r'^accounts/password/reset/$', PasswordResetView.as_view(),
+            name='account_change_password'),
+    re_path(r'^accounts/password/reset/done/$',
+            PasswordResetDoneView.as_view(), name='account_password_reset_done'),
+    re_path(r"^accounts/password/reset/key/(?P<uidb36>[0-9A-Za-z]+)-(?P<key>.+)/$",
+            PasswordResetFromKeyView.as_view(),
+            name="account_reset_password_from_key"),
+    re_path(r"^password/reset/key/done/$", PasswordResetFromKeyDoneView.as_view(),
+            name="account_reset_password_from_key_done"),
 ]
--- logbook/settings.py
+++ logbook/settings.py
@@ -261,13 +261,25 @@ STATICFILES_DIRS = [
     DJANGO_ROOT + "/static",
 ]
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
-AWS_DEFAULT_ACL = None
-AWS_S3_FILE_OVERWRITE = False
+
 MEDIA_URL = config('MEDIA_URL')
+
+STORAGES = {
+    "default": {
+        "BACKEND": "storages.backends.s3.S3Storage",
+        "OPTIONS": {
+            "bucket_name": config('AWS_STORAGE_BUCKET_NAME'),
+            "access_key": config('AWS_SECRET_ACCESS_KEY'),
+            "secret_key": config('AWS_SECRET_ACCESS_KEY'),
+            "default_acl": None,
+            "file_overwrite": False
+        },
+    },
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+    },
+}
+
 
 CACHES = {
     'default': {
