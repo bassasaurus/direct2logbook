2cac27fe8dadb9e826bfc210ae279b8e48241e8e
bassasaurus <clearance.clarence@gmail.com>
2025-07-30 17:14:29 -0400

    migrations, and form tests

--- profile/forms.py
+++ profile/forms.py
@@ -8,3 +8,8 @@ class ProfileForm(forms.ModelForm):
         model = Profile
         fields = ('company', 'medical_issue_date', 'first_class',
                   'second_class', 'third_class', 'over_40')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['medical_issue_date'].required = True
+        self.fields['company'].required = True
