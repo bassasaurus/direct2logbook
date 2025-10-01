306bcb06da2aede0b83d8b6d95c738e2fdd00f5c
bassasaurus <clearance.clarence@gmail.com>
2025-10-01 13:45:05 -0500

    widget tweak

--- signature/forms.py
+++ signature/forms.py
@@ -1,8 +1,20 @@
 from django import forms
 from .models import Signature
+from signature_pad import SignaturePadWidget
 
 
 class SignatureForm(forms.ModelForm):
+
+    signature = forms.CharField(
+        widget=SignaturePadWidget(
+            dotSize=2.5,
+            minWidth=1.0,
+            maxWidth=4.0,
+            backgroundColor="rgb(240, 240, 240)",
+            penColor="rgb(0, 0, 0)"
+        )
+    )
+
     class Meta:
         model = Signature
         exclude = ['user']
